5d20c36b73f711a2d30c6b7abee9c727cfebb21c
Simon Mirco <simon.mirco@rackspace.com>
2015-11-15 23:03:27 +1100

    Working setup.py, oops

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
-    name='alpha-lambda',
-    version='0.1',
-    py_modules=['alphacli'],
+    name='alpha',
+    version='0.2',
+    url='https://github.com/boxidau/alpha',
+    author='Simon Mirco',
+    description='Continuous delivery for AWS Lambda functions',
+    py_modules=['alpha', 'alphacli'],
     include_package_data=True,
-    install_requires=[
-        'click'
-    ],
+    install_requires=required,
     entry_points='''
         [console_scripts]
         alpha=alphacli:cli
